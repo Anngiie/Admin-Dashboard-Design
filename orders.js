29f6d188af2efbdb3d22d0c7e041b0aef35bae01
Anngiie <andjela.casic@gmail.com>
2022-08-27 00:22:37 +0200

    First commit

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'CRIMGTEY usb mini Adapter',
+        productNumber: '36378',
+        paymentStatus: 'Refused',
+        shipping: 'Declined',
+    },
+    {
+        productName: 'Wireless Argonomic Computer Mouse',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Portable Gatley mini keyboard',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '22321',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName: 'Gatley colorful computer mouse',
+        productNumber: '81499',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'DJIRZZZA Air 2kP',
+        productNumber: '00482',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    }
+]
